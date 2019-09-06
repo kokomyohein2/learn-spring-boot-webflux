f1fe0132ceda48d28b0b555c9b0d0a5d3be99e0f
KoKoMyoHeinHtet <kokomyoheinhtet@ascendcorp.com>
2019-09-06 11:19:29 +0700

    15 Flux/Mono - Exploring Factory methods

--- src/test/java/com/learnreactivespring/fluxandmonoplayground/FluxAndMonoFactoryTest.java
+++ src/test/java/com/learnreactivespring/fluxandmonoplayground/FluxAndMonoFactoryTest.java
@@ -2,10 +2,12 @@ package com.learnreactivespring.fluxandmonoplayground;
 
 import org.junit.Test;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Supplier;
 
 public class FluxAndMonoFactoryTest {
 
@@ -21,4 +23,59 @@ public class FluxAndMonoFactoryTest {
                 .expectNext("adam", "anna", "jack", "jenny")
                 .verifyComplete();
     }
+
+    @Test
+    public void fluxUsingArray() {
+
+        String[] names = new String[]{"adam", "anna", "jack", "jenny"};
+
+        Flux<String> namesFlux = Flux.fromArray(names);
+        StepVerifier.create(namesFlux)
+                .expectNext("adam", "anna", "jack", "jenny")
+                .verifyComplete();
+    }
+
+    @Test
+    public void fluxUsingString() {
+
+        Flux<String> namesFlux = Flux.fromStream(names.stream());
+
+        StepVerifier.create(namesFlux)
+                .expectNext("adam", "anna", "jack", "jenny")
+                .verifyComplete();
+    }
+
+    @Test
+    public void monoUsingJustOrEmpty() {
+
+        Mono<String> mono = Mono.justOrEmpty(null); //Mono.Empty();
+
+        StepVerifier.create(mono.log())
+                .verifyComplete();
+    }
+
+    @Test
+    public void monoUsingSupplier() {
+        Supplier<String> stringSupplier = () -> "adam";
+
+        Mono<String> stringMono = Mono.fromSupplier(stringSupplier);
+
+        System.out.println(stringSupplier.get());
+
+        StepVerifier.create(stringMono.log())
+                .expectNext("adam")
+                .verifyComplete();
+    }
+
+    @Test
+    public void fluxUsingRange() {
+
+        Flux<Integer> integerFlux = Flux.range(1, 5).log();
+
+        StepVerifier.create(integerFlux)
+                .expectNext(1, 2, 3, 4, 5)
+                .verifyComplete();
+    }
+
+
 }
