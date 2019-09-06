89562290d7d203db0ff4e6b2a50e72587ae50b76
KoKoMyoHeinHtet <kokomyoheinhtet@ascendcorp.com>
2019-09-06 15:45:00 +0700

    21 Infinite Reactive Stream

---
+++ src/test/java/com/learnreactivespring/fluxandmonoplayground/FluxAndMonoWithTimeTest.java
@@ -0,0 +1,68 @@
+package com.learnreactivespring.fluxandmonoplayground;
+
+import org.junit.Test;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import java.time.Duration;
+
+public class FluxAndMonoWithTimeTest {
+
+    @Test
+    public void infiniteSequence() {
+
+        Flux<Long> infiniteFlux = Flux.interval(Duration.ofMillis(200))
+                .log();
+
+        infiniteFlux.subscribe((element) -> System.out.println("Value is : " + element));
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void infiniteSequenceTest() {
+        Flux<Long> finiteFlux = Flux.interval(Duration.ofMillis(100))
+                .take(3)
+                .log();
+
+        StepVerifier.create(finiteFlux)
+                .expectSubscription()
+                .expectNext(0L, 1L, 2L)
+                .verifyComplete();
+
+
+    }
+
+    @Test
+    public void infiniteSequenceMap() {
+
+        Flux<Integer> finiteFlux = Flux.interval(Duration.ofMillis(100))
+                .map(l -> new Integer(l.intValue()))
+                .take(3)
+                .log();
+
+        StepVerifier.create(finiteFlux)
+                .expectSubscription()
+                .expectNext(0, 1, 2)
+                .verifyComplete();
+    }
+
+    @Test
+    public void infiniteSequenceMap_withDelay() {
+
+        Flux<Integer> finiteFlux = Flux.interval(Duration.ofMillis(100))
+                .delayElements(Duration.ofSeconds(1))
+                .map(l -> new Integer(l.intValue()))
+                .take(3)
+                .log();
+
+        StepVerifier.create(finiteFlux)
+                .expectSubscription()
+                .expectNext(0, 1, 2)
+                .verifyComplete();
+    }
+}
