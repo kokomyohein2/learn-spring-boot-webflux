9e49375bd64bbb60cf4f0dc6846afed9635a9e3f
KoKoMyoHeinHtet <kokomyoheinhtet@ascendcorp.com>
2019-09-09 17:51:42 +0700

    52 Get All Items - Junit Test using WebTestClient Part 1

---
+++ src/test/java/com/learnreactivespring/controller/v1/ItemControllerTest.java
@@ -0,0 +1,66 @@
+package com.learnreactivespring.controller.v1;
+
+import com.learnreactivespring.constants.ItemConstants;
+import com.learnreactivespring.document.Item;
+import com.learnreactivespring.repository.ItemReactiveRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+
+import java.util.Arrays;
+import java.util.List;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@DirtiesContext
+@AutoConfigureWebTestClient
+@ActiveProfiles("test")
+public class ItemControllerTest {
+
+    @Autowired
+    WebTestClient webTestClient;
+
+    @Autowired
+    ItemReactiveRepository itemReactiveRepository;
+
+    public List<Item> data() {
+
+        return Arrays.asList(
+                new Item(null, "Samsung TV", 400.0),
+                new Item(null, "LG TV", 299.99),
+                new Item(null, "Apple Watch", 149.99),
+                new Item("ABC", "Apple Airpods", 149.99));
+    }
+
+    @Before
+    public void setUp() {
+
+        itemReactiveRepository.deleteAll()
+                .thenMany(Flux.fromIterable(data()))
+                .flatMap(itemReactiveRepository::save)
+                .doOnNext(item -> {
+                    System.out.println("Inserted item is : " + item);
+                })
+                .blockLast();
+    }
+
+    @Test
+    public void getAllItems() {
+        webTestClient.get().uri(ItemConstants.ITEM_END_POINT_V1)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
+                .expectBodyList(Item.class)
+                .hasSize(4);
+    }
+
+}
