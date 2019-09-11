f61ecf0813e057a69533c008ac089e0075bd096b
KoKoMyoHeinHtet <kokomyoheinhtet@ascendcorp.com>
2019-09-11 18:20:41 +0700

    84 build the Stream Endpoint using Controller

--- src/main/java/com/learnreactivespring/initialize/ItemDataInitializer.java
+++ src/main/java/com/learnreactivespring/initialize/ItemDataInitializer.java
@@ -2,7 +2,9 @@ package com.learnreactivespring.initialize;
 
 import com.learnreactivespring.document.Item;
 import com.learnreactivespring.document.ItemCapped;
+import com.learnreactivespring.repository.ItemReactiveCappedRepository;
 import com.learnreactivespring.repository.ItemReactiveRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
@@ -11,9 +13,11 @@ import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
 
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
 
+@Slf4j
 @Component
 @Profile(("!test"))
 public class ItemDataInitializer implements CommandLineRunner {
@@ -24,11 +28,15 @@ public class ItemDataInitializer implements CommandLineRunner {
     @Autowired
     MongoOperations mongoOperations;
 
+    @Autowired
+    ItemReactiveCappedRepository itemReactiveCappedRepository;
+
     @Override
     public void run(String... args) throws Exception {
 
         initialDataSetUp();
         createCappedCollection();
+        dataSetupforCappedCollection();
     }
 
     private void createCappedCollection() {
@@ -45,6 +53,16 @@ public class ItemDataInitializer implements CommandLineRunner {
                 new Item(null, "LG TV", 299.99),
                 new Item(null, "Apple Watch", 149.99),
                 new Item("ABC", "Apple Airpods", 149.99));
+    }
+
+    public void dataSetupforCappedCollection() {
+
+        Flux<ItemCapped> itemCappedFlux = Flux.interval(Duration.ofSeconds(1))
+                .map(i -> new ItemCapped(null, "Random item " + i, (100.00 + i)));
+
+        itemReactiveCappedRepository
+                .insert(itemCappedFlux)
+                .subscribe(itemCapped -> log.info("Inserted Item is " + itemCapped));
     }
 
     private void initialDataSetUp() {
